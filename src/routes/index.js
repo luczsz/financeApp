505e746f61e354da4e4ce0e55dcd8311a03717c2
luczsdev <francisco.lucs@gmail.com>
2023-03-12 11:35:57 -0300

    PRIMEIROS PASSOS

---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Home from '../pages/Home';
+
+
+export default function Routes() {
+
+    const AppStack = createNativeStackNavigator();
+
+ return (
+   <AppStack.Navigator>
+        <AppStack.Screen
+            name='Home'
+            component={Home}
+        />
+   </AppStack.Navigator>
+  );
+}
